3ad4bbfa13cb527f072472f74a95e25118db6502
Matt Greenwolfe <solargreen@mindspring.com>
2015-10-19 20:14:02 -0400

    deleted correctNullStatus?

--- server/repairNullStatus.js
+++
@@ -1,6 +0,0 @@
-Meteor.startup(function () {
-  ActivityStatuses.find().forEach(function(status) {
-    if (!status.level)
-      ActivityStatuses.update(status._id,{$set: {level: 'nostatus'}});
-  });
-});
